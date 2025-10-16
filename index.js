e1ca1fac922774f13c64b7deb671f283ebb7517f
Aman kumar <amankumar105541@gmail.com>
2025-10-16 23:22:04 +0530

    created server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("fdfd0");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/hero',(req,res)=>{
+    res.send('heroo is here');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is listening on port 3000');
+});
